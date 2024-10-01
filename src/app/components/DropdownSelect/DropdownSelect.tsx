a5b1f43c6526d6754a45d58b0ca920c4916e3903
Kidongg <dlrlehd7@daum.net>
2024-10-01 20:47:32 +0900

    feat: 드롭다운 팝오버 UI

--- src/app/components/DropdownSelect/DropdownSelect.tsx
+++ src/app/components/DropdownSelect/DropdownSelect.tsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import { FaCaretDown } from 'react-icons/fa';
 import { FaCaretUp } from 'react-icons/fa';
 
+import DropdownSelectPopover from '@/app/components/DropdownSelectPopover/DropdownSelectPopover';
+
 import styles from './DropdownSelect.module.scss';
 
 export default function DropdownSelect({ text }: Readonly<{ text: string }>) {
@@ -17,8 +19,13 @@ export default function DropdownSelect({ text }: Readonly<{ text: string }>) {
 
   return (
     <div className={`${styles.container} ${isClicked ? styles.clicked : ''}`} onClick={handleOnClickDropdown}>
-      <span className={styles.text}>{text}</span>
-      <span className={styles.icon}>{isClicked ? <FaCaretUp /> : <FaCaretDown />}</span>
+      <div className={styles.button}>
+        <span className={styles.text}>{text}</span>
+        <span className={styles.icon}>{isClicked ? <FaCaretUp /> : <FaCaretDown />}</span>
+      </div>
+      <div className={styles.popover}>
+        <DropdownSelectPopover />
+      </div>
     </div>
   );
 }
