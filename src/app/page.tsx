0ebd6e0ebae05d53d79c99138519c4ae867f6177
Kidongg <dlrlehd7@daum.net>
2024-10-11 00:54:10 +0900

    refactor: 메인 페이지 코드 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import DropdownSelect from '@/app/(main)/components/DropdownSelect/DropdownSelect';
-
-const items = ['전체', '공제 예정', '공제 진행중', '공제 종료'];
-const items2 = ['최신순', '오래된순', '낮은 가격순', '높은 가격순'];
-
 export default function Home() {
-  return (
-    <main>
-      <DropdownSelect items={items} />
-      <DropdownSelect items={items2} />
-    </main>
-  );
+  return <main>메인 페이지</main>;
 }
