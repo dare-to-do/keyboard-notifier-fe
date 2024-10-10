31d4f1dc6544c6198d44de0a81038824e57a299c
Kidongg <dlrlehd7@daum.net>
2024-10-10 23:40:01 +0900

    refactor: isPopover을 isOpenDropdown으로 변경

--- src/app/(main)/components/DropdownSelect/DropdownSelect.tsx
+++ src/app/(main)/components/DropdownSelect/DropdownSelect.tsx
@@ -14,33 +14,33 @@ type ItemsProps = {
 };
 
 const DropdownSelect = ({ items }: ItemsProps) => {
-  const [isPopover, setIsPopover] = useState(false);
+  const [isOpenDropdown, setIsOpenDropdown] = useState(false);
   const [clickedItemText, setClickedItemText] = useState(items[0].text);
 
   // 버튼 클릭 시 드롭다운으로 팝오버 표시
   const handleOnClickDropdown = () => {
-    setIsPopover(!isPopover);
+    setIsOpenDropdown(!isOpenDropdown);
   };
 
   // 드롭다운 메뉴 아이템 클릭 시 텍스트 색상 변경
   const handleOnClickItem = (text: string) => {
     setClickedItemText(text);
-    setIsPopover(false);
+    setIsOpenDropdown(false);
   };
 
   return (
     <div className={styles.container}>
-      <div className={`${styles.button} ${isPopover ? styles.is_popover : ''}`} onClick={handleOnClickDropdown}>
+      <div className={`${styles.button} ${isOpenDropdown ? styles.is_popover : ''}`} onClick={handleOnClickDropdown}>
         <span className={styles.text}>{clickedItemText}</span>
         <span className={styles.icon}>
-          {isPopover ? (
+          {isOpenDropdown ? (
             <img src="/assets/icons/accordian_fo.png" alt="accordian_fo" width="24px" height="24px" />
           ) : (
             <img src="/assets/icons/accordian_en.png" alt="accordian_en" width="24px" height="24px" />
           )}
         </span>
       </div>
-      {isPopover && (
+      {isOpenDropdown && (
         <ul className={styles.popover}>
           {items.map((item) => (
             <li
