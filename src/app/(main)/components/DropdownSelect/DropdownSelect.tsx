f432e84a86ca3abc5756b6853083d81c94f21b10
Kidongg <dlrlehd7@daum.net>
2024-10-11 00:17:41 +0900

    refactor: items 타입을 string[]으로 변경

--- src/app/(main)/components/DropdownSelect/DropdownSelect.tsx
+++ src/app/(main)/components/DropdownSelect/DropdownSelect.tsx
@@ -4,18 +4,13 @@ import { useState } from 'react';
 
 import styles from './DropdownSelect.module.scss';
 
-type Items = {
-  text: string;
-  key: string;
-};
-
 type ItemsProps = {
-  items: Items[];
+  items: string[];
 };
 
 const DropdownSelect = ({ items }: ItemsProps) => {
   const [isOpenDropdown, setIsOpenDropdown] = useState(false);
-  const [clickedItemText, setClickedItemText] = useState(items[0].text);
+  const [clickedItemText, setClickedItemText] = useState(items[0]);
 
   // 버튼 클릭 시 드롭다운으로 팝오버 표시
   const handleOnClickDropdown = () => {
@@ -45,13 +40,13 @@ const DropdownSelect = ({ items }: ItemsProps) => {
         </span>
       </div>
       <ul className={`${styles.dropdown} ${isOpenDropdown ? styles.show : styles.hide}`}>
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className={`${styles.item} ${item.text === clickedItemText ? styles.is_clicked_item : ''}`}
-            onClick={() => handleOnClickItem(item.text)}
-            key={item.key}
+            className={`${styles.item} ${item === clickedItemText ? styles.is_clicked_item : ''}`}
+            onClick={() => handleOnClickItem(item)}
+            key={index}
           >
-            {item.text}
+            {item}
           </li>
         ))}
       </ul>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 import DropdownSelect from '@/app/(main)/components/DropdownSelect/DropdownSelect';
 
-const items = [
-  { text: '전체', key: '1' },
-  { text: '공제 예정', key: '2' },
-  { text: '공제 진행중', key: '3' },
-  { text: '공제 종료', key: '4' },
-];
-
-const items2 = [
-  { text: '최신순', key: '1' },
-  { text: '오래된순', key: '2' },
-  { text: '낮은 가격순', key: '3' },
-  { text: '높은 가격순', key: '4' },
-];
+const items = ['전체', '공제 예정', '공제 진행중', '공제 종료'];
+const items2 = ['최신순', '오래된순', '낮은 가격순', '높은 가격순'];
 
 export default function Home() {
   return (
