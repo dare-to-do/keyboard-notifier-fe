0bd1bb69133e8bde9b6273e718812f85569a1e57
Kidongg <dlrlehd7@daum.net>
2024-10-10 23:38:12 +0900

    fix: 컴포넌트 왼쪽 잘리는 버그 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
 import DropdownSelect from '@/app/(main)/components/DropdownSelect/DropdownSelect';
 
 const items = [
-  { text: '메뉴1', key: 'menu1' },
-  { text: '가나다라', key: 'menu2' },
-  { text: '마바사아자차', key: 'menu3' },
+  { text: '전체', key: '1' },
+  { text: '공제 예정', key: '2' },
+  { text: '공제 진행중', key: '3' },
+  { text: '공제 종료', key: '4' },
+];
+
+const items2 = [
+  { text: '최신순', key: '1' },
+  { text: '오래된순', key: '2' },
+  { text: '낮은 가격순', key: '3' },
+  { text: '높은 가격순', key: '4' },
 ];
 
 export default function Home() {
   return (
     <main>
       <DropdownSelect items={items} />
+      <DropdownSelect items={items2} />
     </main>
   );
 }
