33c5d5186bfb154f251033dead4a8119b2d9ddd8
Kidongg <dlrlehd7@daum.net>
2025-02-04 00:53:09 +0900

    feat: 새로고침 시 슬라이드 안보이는 문제 해결

--- src/app/(detail)/components/GBItemCarousel/GBItemCarousel.tsx
+++ src/app/(detail)/components/GBItemCarousel/GBItemCarousel.tsx
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import Slider from 'react-slick';
 
 import styles from './GBItemCarousel.module.scss';
+import 'slick-carousel/slick/slick.css';
 
 const cx = classNames.bind(styles);
 
@@ -12,26 +13,9 @@ type GBItemCarouselProps = {
 };
 
 const GBItemCarousel = ({ imageUrlList }: GBItemCarouselProps) => {
-  const settings = {
-    arrows: false,
-    dots: true,
-    appendDots: (dots: React.ReactNode) => (
-      <div
-        style={{
-          bottom: '20px',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          width: '50%',
-        }}
-      >
-        <ul style={{ margin: '0px' }}> {dots} </ul>
-      </div>
-    ),
-  };
-
   return (
     <section className={cx('container')}>
-      <Slider {...settings}>
+      <Slider slidesToShow={1} centerMode centerPadding="0px" arrows={false}>
         {imageUrlList.map((url, index) => (
           <img className={cx('image')} src={url} key={index} alt="image" width="584px" height="520px" />
         ))}
