98cad52a6bda164688135bd99592a6b34fbc8146
Kidongg <dlrlehd7@daum.net>
2025-02-22 23:03:35 +0900

    feat: 스크롤 시 데이터 단건 함수 실행을 위한 디바운스 함수 생성

---
+++ src/app/(shared)/utils/debounce.ts
@@ -0,0 +1,9 @@
+export const debounce = (func: () => void, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return () => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func();
+    }, delay);
+  };
+};
