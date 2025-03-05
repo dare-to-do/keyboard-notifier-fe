e05dcf2539764806314a0fc6d0d31c8a1a3673f1
Kidongg <dlrlehd7@daum.net>
2025-03-05 22:42:00 +0900

    feat: 메인 페이지에서 이미지 가져오는 경로 변경

--- src/app/components/GBItem/GBitem.tsx
+++ src/app/components/GBItem/GBitem.tsx
@@ -42,7 +42,7 @@ const GBItem = ({ name, price, unit, startDate, endDate, imageUrl, status, categ
       })}
       role="button"
       style={{
-        backgroundImage: `linear-gradient(180deg, rgba(23, 23, 25, 0) 33.09%, rgba(23, 23, 25, 0.18) 53.58%, rgba(23, 23, 25, 0.3) 74.45%), url(${imageUrl[0]})`,
+        backgroundImage: `linear-gradient(180deg, rgba(23, 23, 25, 0) 33.09%, rgba(23, 23, 25, 0.18) 53.58%, rgba(23, 23, 25, 0.3) 74.45%), url(/api/image-proxy?url=${encodeURIComponent(imageUrl[0])})`,
       }}
       onClick={() => moveToDetailPage(id)}
       onMouseOver={() => setIsHover(true)}
